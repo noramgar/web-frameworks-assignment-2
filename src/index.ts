c792ac796c5f2f85b523b1a8bc512f80fe316f58
Noe <noramgar@protonmail.com>
2020-09-18 13:02:28 -0400

    add user class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
 import express from 'express';
 import path from 'path';
 
+import User from './resources/user/User'
+
 const app = express()
 const port = 3000
 
+//app.use('/', express.static('/public/help.html'));
+//app.use('/public', express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname + "/public", {
+    index: false, 
+    immutable: true, 
+    cacheControl: true,
+    maxAge: "30d"
+}));
+
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/help.html'))
+  //res.sendFile(path.join(__dirname + '/public/help.html'))
+  res.sendFile('./public/help.html', { root: __dirname });
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+let bob = new User('bob42', 'Bob', 'Griffin', 'bob@mail.com', 'pass123');
+bob.save();
+
+console.log(bob.lastName);
---
+++ src/resources/user/User.ts
@@ -0,0 +1,34 @@
+export default class User {
+    username: String
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+
+    private static users: User[] = [];
+
+    constructor(username: String,firstName: String,lastName: String,email: String,password: String) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    save() {
+        User.users.push(this)
+    }
+
+    static userIdExists(username: String) {
+        for (const user of User.users) {
+            if (user.username === username) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    static deleteUser(username: String) {
+        User.users = User.users.filter(user => user.username !== username)
+    }
+}
